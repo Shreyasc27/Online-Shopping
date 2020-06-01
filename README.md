ba089f7fc89b42d2cc2ea57c61bbde94c96bac98
Shreyas Chaudhari <shreyas.chaudhari@n26.com>
2020-06-01 16:12:20 +0530

    Update: ReadMe with api end point details

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Mern-Shopping-List
+
+## Api End Points
+
+#### 1. GET - Get all items
+![GET Request](/readMeImages/GET.png)
+
+#### 2. POST - Create an item
+![POST Request](/readMeImages/POST.png)
+
+#### 3. DELETE - Delete an item
+![DELETE Request](/readMeImages/DELETE.png)
+
+#### 4. PUT - Update an item
+![PUT Request](/readMeImages/PUT.png)
--- routes/api/items.js
+++ routes/api/items.js
@@ -25,5 +25,23 @@ router.post('/', (req, res) => {
     .then(item => res.json(item));
 });
 
+// @route  DELETE api/items/:id
+// @desc   Delete A Item
+// @access Public 
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+// @route PUT api/items/:id
+// @desc Update An Item
+// @access Public
+router.put("/:id", (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.update({ $set: { name: req.body.name } }).then(()=> res.json( {success: true})))
+    .catch(err => res.status(404).json({ success: false}));
+  });
+
 module.exports = router;
 
