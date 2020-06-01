52cf0f355b6a16a5d08a8c6f8194c2ff202137bb
Shreyas Chaudhari <shreyas.chaudhari@n26.com>
2020-06-01 13:46:34 +0530

    Added GET & POST Requests

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://shreyas:Test@123@cluster0-nfedh.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const Item = require('../../models/Item');
+
+// @route  GET api/items
+// @desc   Get All Items
+// @access Public 
+express.Router().get('/', (req, res) => {
+    Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+});
+
+// @route  POST api/items
+// @desc   Create an Item
+// @access Public 
+express.Router().post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+});
+
+module.exports = express.Router();
+
