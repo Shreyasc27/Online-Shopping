1618a507c6b9cee0cb8972b4dff945a55b016d71
Shreyas Chaudhari <shreyas.chaudhari@n26.com>
2020-06-02 03:56:37 +0530

    Add: Config for deploying on Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const items = require('./routes/api/items');
 const db = require('./config/key').mongoURI;
+const path = require('path');
 
 var app = express();
 app.use(express.json());
@@ -12,6 +13,13 @@ mongoose
 .catch(err => console.log(err))
 
 app.use('/api/items', items);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.set('port', process.env.PORT || port);
